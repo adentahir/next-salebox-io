b97d2062c5c23163e99adedf8295cde9d3d7e15a
Aden Tahir <aden.tahir@carbonteq.com>
2024-05-13 14:33:01 +0500

    configurations for the POS application

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import fs from 'fs';
+
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.devServer = {
+        https: {
+            key: fs.readFileSync('./certificates/localhost.key'),
+            cert: fs.readFileSync('./certificates/localhost.crt'),
+        },
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
